e04239b91d434e12a3edd188f653b8dd3901a5ba
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-16 23:41:42 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,26 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        mut h: [usize; n]
     }
+    let mut ans = 0;
+    loop {
+        if h.clone().into_iter().max().unwrap() == 0 {
+            break;
+        }
+        let mut i = 0;
+        while i < n {
+            if h[i] == 0 {
+                i += 1;
+            } else {
+                ans += 1;
+                while i < n && h[i] > 0 {
+                    h[i] -= 1;
+                    i += 1;
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
